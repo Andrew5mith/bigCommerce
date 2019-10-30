684243569ffd4636d42f7214eff6829c05484c3c
Andrew5mith <44306455+Andrew5mith@users.noreply.github.com>
2019-10-30 00:41:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,3 +11,15 @@ app.post('/webhooks', function (req, res) {
 app.listen(3000, function () {
     console.log('Listening for webhooks on port 3000')
 })
+
+let http = require('http');
+ 
+let handleRequest = (request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    response.write('OK');
+    response.end();
+};
+ 
+http.createServer(handleRequest).listen(8000);
